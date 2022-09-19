5e65cb9d8929cbde56b086f8c10ade0f1be27e49
MJWORKS <mjworks2020@outlook.com>
2022-09-19 16:56:51 -0300

    Added new changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,11 @@
+import Head from "next/head";
+
 export default function Home() {
   return (
     <>
+      <Head>
+        <title>BarberPro</title>
+      </Head>
       <div className="">
         <h1>Teste</h1>
       </div>
