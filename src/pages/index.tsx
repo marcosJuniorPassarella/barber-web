c76491aee08e5d85b9569ce441ce1f418140fb13
MJWORKS <mjworks2020@outlook.com>
2022-09-20 10:36:14 -0300

    Created Login Page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import { Flex, Text } from "@chakra-ui/react";
 
 export default function Home() {
   return (
@@ -6,9 +7,14 @@ export default function Home() {
       <Head>
         <title>BarberPro</title>
       </Head>
-      <div className="">
-        <h1>Teste</h1>
-      </div>
+      <Flex
+        background="barber.900"
+        height="100vh"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Text fontSize="30" color="button.gray">Página Home</Text>
+      </Flex>
     </>
   );
 }
