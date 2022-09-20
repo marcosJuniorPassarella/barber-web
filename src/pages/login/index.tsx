044dfd68ddc666db8e14f2335ec2f298bfcd7439
MJWORKS <mjworks2020@outlook.com>
2022-09-20 15:27:49 -0300

    Added axios nookies and jwt-decode lib

--- src/pages/login/index.tsx
+++ src/pages/login/index.tsx
@@ -1,17 +1,19 @@
-import { useState } from "react";
+import { useState, useContext } from "react";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import { Flex, Text, Center, Input, Button } from "@chakra-ui/react";
 import logoImg from "../../../public/images/logo.svg";
+import { AuthContext } from "../../context/AuthContext";
 
 export default function Login() {
+  const { signIn } = useContext(AuthContext);
+
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  function handleLogin() {
-    console.log(email)
-    console.log(password)
+  async function handleLogin() {
+    await signIn({ email, password });
   }
 
   return (
