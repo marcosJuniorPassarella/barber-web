487e8b9b786c01c02d7ce226b0a8268ba4ebe89a
MJWORKS <mjworks2020@outlook.com>
2022-09-20 15:55:27 -0300

    Added signOut method in the AuthContext

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,6 @@
 import { createContext, ReactNode, useState } from "react";
+import Router from "next/router";
+import { destroyCookie } from "nookies";
 
 interface AuthContextData {
   user: UserProps;
@@ -29,6 +31,16 @@ interface SignInProps {
 }
 
 export const AuthContext = createContext({} as AuthContextData);
+
+export function signOut() {
+  console.log("Error Logout");
+  try {
+    destroyCookie(null, "@barber.token", { path: "/" });
+    Router.push("/login");
+  } catch (error) {
+    console.log("Erro ao sair");
+  }
+}
 
 export function AuthProvider({ children }: AuthProviderProps) {
   const [user, setUser] = useState<UserProps>();
