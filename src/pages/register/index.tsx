277c1d2e2cf3c52073fac0d44e5fca6483ca5d67
MJWORKS <mjworks2020@outlook.com>
2022-09-20 10:40:30 -0300

    Created register page

---
+++ src/pages/register/index.tsx
@@ -0,0 +1,77 @@
+import Head from "next/head";
+import Image from "next/image";
+import Link from "next/link";
+import { Flex, Text, Center, Input, Button } from "@chakra-ui/react";
+import logoImg from "../../../public/images/logo.svg";
+
+export default function Register() {
+  return (
+    <>
+      <Head>
+        <title>BarberPro - Crie sua conta</title>
+      </Head>
+      <Flex
+        background="barber.900"
+        height="100vh"
+        alignItems="center"
+        justifyContent="center"
+      >
+        <Flex width="640" direction="column" p="14" rounded="8">
+          <Center p="4">
+            <Image
+              src={logoImg}
+              quality={100}
+              width="300"
+              objectFit="fill"
+              alt="Logo BarberPro"
+            ></Image>
+          </Center>
+          <Input
+            background="barber.400"
+            variant="filled"
+            size="lg"
+            placeholder="Nome da barbearia"
+            type="email"
+            mb={3}
+            color="button.gray"
+          />
+          <Input
+            background="barber.400"
+            variant="filled"
+            size="lg"
+            placeholder="email@barber.com"
+            type="email"
+            mb={6}
+            color="button.gray"
+          />
+          <Input
+            background="barber.400"
+            variant="filled"
+            size="lg"
+            placeholder="*******"
+            type="text"
+            mb={6}
+            color="button.gray"
+          />
+          <Button
+            background="button.cta"
+            mb={6}
+            color="gray.900"
+            size="lg"
+            _hover={{ bg: "#ffb13e" }}
+          >
+            Cadastrar
+          </Button>
+
+          <Center mt={2}>
+            <Link href="/login">
+              <Text color="button.gray" cursor="pointer">
+                Já possui uma conta? <strong>Faça login</strong>
+              </Text>
+            </Link>
+          </Center>
+        </Flex>
+      </Flex>
+    </>
+  );
+}
