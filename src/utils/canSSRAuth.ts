00035b27a733d107d78913ed30106748f30ff906
MJWORKS <mjworks2020@outlook.com>
2022-09-21 18:46:44 -0300

    Added canSSRAuth file

---
+++ src/utils/canSSRAuth.ts
@@ -0,0 +1,39 @@
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+} from "next";
+import { parseCookies, destroyCookie } from "nookies";
+import { AuthTokenError } from "../services/errors/AuthTokenError";
+
+export function canSSRAuth<P>(fn: GetServerSideProps<P>) {
+  return async (
+    ctx: GetServerSidePropsContext
+  ): Promise<GetServerSidePropsResult<P>> => {
+    const cookies = parseCookies(ctx);
+    const token = cookies["@barber.token"];
+
+    if (!token) {
+      return {
+        redirect: {
+          destination: "/login",
+          permanent: false,
+        },
+      };
+    }
+
+    try {
+      return await fn(ctx);
+    } catch (error) {
+      if (error instanceof AuthTokenError) {
+        destroyCookie(ctx, "@barber.token", { path: "/" });
+        return {
+          redirect: {
+            destination: "/",
+            permanent: false,
+          },
+        };
+      }
+    }
+  };
+}
