b6cabc0ac1fa9ac6490dd0ca1d7782d9053c2fa4
MJWORKS <mjworks2020@outlook.com>
2022-10-10 19:51:05 -0300

    Added interface updates in the id haircut page

--- src/pages/haircuts/[id].tsx
+++ src/pages/haircuts/[id].tsx
@@ -1,17 +1,46 @@
 import Head from "next/head";
 import { Sidebar } from "../../components/sidebar/index";
-import { Flex, Heading } from "@chakra-ui/react";
+import { Flex, Heading, useMediaQuery, Button } from "@chakra-ui/react";
+import { FiChevronLeft } from "react-icons/fi";
+import Link from "next/link";
 
 export default function EditHaircut() {
+  const [isMobile] = useMediaQuery("(max-width: 500px)");
   return (
     <>
       <Head>
         <title>Editar modelo de corte - BarberPro</title>
       </Head>
       <Sidebar>
-        <Flex>
-          <Flex>
-            <Heading>Editar corte</Heading>
+        <Flex
+          direction="column"
+          alignItems="flex-start"
+          justifyContent="flex-start"
+        >
+          <Flex
+            direction={isMobile ? "column" : "row"}
+            w="100%"
+            alignItems={isMobile ? "flex-start" : "center"}
+            justifyContent="flex-start"
+            mb={isMobile ? 4 : 0}
+          >
+            <Link href="/haircuts">
+              <Button
+                mr={3}
+                p={4}
+                display="flex"
+                alignItems="center"
+                justifyContent="center"
+                background="barber.400"
+                color="white"
+              >
+                <FiChevronLeft size={24} color="#fff" />
+                Voltar
+              </Button>
+            </Link>
+            <Heading fontSize={isMobile ? "22px" : "3xl"} color="white">
+              Editar corte
+            </Heading>
           </Flex>
         </Flex>
       </Sidebar>
