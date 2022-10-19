fc4d40453c0351a3bc6181a05e7535c8873b60ca
MJWORKS <mjworks2020@outlook.com>
2022-10-19 15:04:16 -0300

    Added new updates in the dashboard screen

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,17 +1,79 @@
 import Head from "next/head";
-import { Flex, Text } from "@chakra-ui/react";
+import Link from "next/link";
+import {
+  Flex,
+  Heading,
+  Button,
+  Link as ChakraLink,
+  useMediaQuery,
+  Text,
+} from "@chakra-ui/react";
+import { IoMdPerson } from "react-icons/io";
 import { canSSRAuth } from "../../utils/canSSRAuth";
 import { Sidebar } from "../../components/sidebar";
 
 export default function Dashboard() {
+  const [isMobile] = useMediaQuery("(max-width: 500px)");
+
   return (
     <>
       <Head>
         <title>BarberPro - Dashboard</title>
       </Head>
       <Sidebar>
-        <Flex>
-          <Text>Bem vindo ao dashboard!</Text>
+        <Flex direction="column" align="flex-start" justify="flex-start">
+          <Flex
+            width="100%"
+            direction="row"
+            align="center"
+            justify="flex-start"
+          >
+            <Heading color="white" fontSize="3xl" mt={4} mb={4} mr={4}>
+              Agenda
+            </Heading>
+            <Link href="/new">
+              <Button>Registrar</Button>
+            </Link>
+          </Flex>
+
+          <ChakraLink
+            w="100%"
+            m={0}
+            p={0}
+            mt={1}
+            bg="transparent"
+            style={{ textDecoration: "none" }}
+          >
+            <Flex
+              w="100%"
+              direction={isMobile ? "column" : "row"}
+              p={4}
+              rounded={4}
+              mb={4}
+              bg="barber.400"
+              justify="space-between"
+              align={isMobile ? "flex-start" : "center"}
+            >
+              <Flex
+                direction="row"
+                mb={isMobile ? 2 : 0}
+                align="center"
+                justify="center"
+              >
+                <IoMdPerson size={28} color="orange" />
+                <Text fontWeight="bold" color="white" ml={4} noOfLines={1}>
+                  Marcos Júnior
+                </Text>
+              </Flex>
+
+              <Text color="white" mb={isMobile ? 2 : 0} fontWeight="bold">
+                Corte completo
+              </Text>
+              <Text color="white" mb={isMobile ? 2 : 0} fontWeight="bold">
+                R$ 59.99
+              </Text>
+            </Flex>
+          </ChakraLink>
         </Flex>
       </Sidebar>
     </>
