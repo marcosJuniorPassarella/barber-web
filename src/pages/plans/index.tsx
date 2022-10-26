e0068abf315f52abdbeecc1b99f4b72994cf85c2
MJWORKS <mjworks2020@outlook.com>
2022-10-26 17:50:50 -0300

    Added new template updates in the plans page

--- src/pages/plans/index.tsx
+++ src/pages/plans/index.tsx
@@ -3,6 +3,8 @@ import { Button, Flex, Heading, Text, useMediaQuery } from "@chakra-ui/react";
 import { Sidebar } from "../../components/sidebar";
 
 export default function Plans() {
+  const [isMobile] = useMediaQuery("(max-width: 500px)");
+
   return (
     <>
       <Head>
@@ -18,6 +20,89 @@ export default function Plans() {
           <Heading color="white" fontSize="3xl" mt={4} mb={4} mr={4}>
             Planos
           </Heading>
+        </Flex>
+
+        <Flex
+          pb={8}
+          maxW="780px"
+          w="100%"
+          direction="column"
+          align="flex-start"
+          justify="flex-start"
+        >
+          <Flex gap={4} w="100%" flexDirection={isMobile ? "column" : "row"}>
+            <Flex
+              rounded={4}
+              p={2}
+              flex={1}
+              bg="barber.400"
+              flexDirection="column"
+            >
+              <Heading
+                textAlign="center"
+                fontSize="2xl"
+                mt={2}
+                mb={3}
+                color="white"
+              >
+                Plano Grátis
+              </Heading>
+              <Text color="white" fontWeight="medium" ml={4} mb={2}>
+                Registrar cortes
+              </Text>
+              <Text color="white" fontWeight="medium" ml={4} mb={2}>
+                Criar apenas 3 modelos de corte
+              </Text>
+              <Text color="white" fontWeight="medium" ml={4} mb={2}>
+                Editar dados do perfil
+              </Text>
+            </Flex>
+            <Flex
+              rounded={4}
+              p={2}
+              flex={1}
+              bg="barber.400"
+              flexDirection="column"
+            >
+              <Heading
+                textAlign="center"
+                fontSize="2xl"
+                mt={2}
+                mb={3}
+                color="#31fb6a"
+              >
+                Plano Premium
+              </Heading>
+              <Text color="white" fontWeight="medium" ml={4} mb={2}>
+                Registrar cortes ilimitados
+              </Text>
+              <Text color="white" fontWeight="medium" ml={4} mb={2}>
+                Criar modelos de corte ilimitados
+              </Text>
+              <Text color="white" fontWeight="medium" ml={4} mb={2}>
+                Editar modelos de corte
+              </Text>
+              <Text color="white" fontWeight="medium" ml={4} mb={2}>
+                Editar dados do perfil
+              </Text>
+              <Text color="white" fontWeight="medium" ml={4} mb={2}>
+                Receber todas atualizações
+              </Text>
+              <Text color="#31fb6a" fontWeight="bold" ml={4} mb={2}>
+                R$ 9,99
+              </Text>
+
+              <Button
+                bg="button.cta"
+                m={2}
+                color="white"
+                _hover={{ bg: "#FFb13e" }}
+                onClick={() => {}}
+              >
+                Se tornar Premium
+              </Button>
+            </Flex>
+          </Flex>
         </Flex>
       </Sidebar>
     </>
